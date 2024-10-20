11b91d052b05b81f9cbdfe591586dc4b0dc92437
kushitha047 <22hr1a3234@mtieat.org>
2024-10-20 18:35:23 +0530

    sample.java

---
+++ sample.java
@@ -0,0 +1,19 @@
+***
+sample program
+***
+class Sample{
+        public static void main(String args[]){
+        //System.out.println("hello world");
+	Dog d=new Dog();
+	d.run();
+        d.cry();
+        d.bark();
+        d.walk();
+        d.sleep();
+	System.out.println(d.name);
+	System.out.println(d.color);
+	System.out.println(d.cost);
+	System.out.println(d.age);
+	System.out.println(d.breed);
+        }
+}
