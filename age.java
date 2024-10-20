9a24c5e0e3e60569d588af22493c86e83f755a4a
kushitha047 <22hr1a3234@mtieat.org>
2024-10-20 18:43:42 +0530

    Create age.java

---
+++ age.java
@@ -0,0 +1,14 @@
+***
+eligible for vote or not using in if class
+***
+class Age
+{
+public static void main(String args[])
+{
+int age=18;
+if (age>=18)
+{
+System.out.println("eligible for vote");
+}
+}
+}
