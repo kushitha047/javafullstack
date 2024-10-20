f2bd63eaec34341ae290fa58c7f201c07411e489
kushitha047 <22hr1a3234@mtieat.org>
2024-10-20 18:32:21 +0530

    Create vote.java

---
+++ vote.java
@@ -0,0 +1,18 @@
+***
+eligible for vote or not
+***
+class Vote
+{
+public static void main(String args[])
+{
+int age=17;
+if(age>=18)
+{
+System.out.println("eligible for vote");
+}
+else
+{
+System.out.println("not eligible for vote");
+}
+}
+}
