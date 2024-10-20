0717a6665a2c1e0934f44b94ee6820d231d80630
kushitha047 <22hr1a3234@mtieat.org>
2024-10-20 18:46:04 +0530

    Create evenodd.java

---
+++ evenodd.java
@@ -0,0 +1,21 @@
+***
+print even and odd number
+***
+class Evenodd
+{
+public static void main(String args[])
+{
+int i=1;
+for(i=1;i<=10;i++)
+{
+if(i%2==0)
+{
+System.out.println(i+" is even number");
+}
+else
+{
+System.out.println(i+" is odd number");
+}
+}
+}
+}
