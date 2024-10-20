2c491094b0073e4093ef89a7d2a89d0df173762a
kushitha047 <22hr1a3234@mtieat.org>
2024-10-20 18:41:15 +0530

    Create dog.java

---
+++ dog.java
@@ -0,0 +1,26 @@
+***
+create properties and methods for dog
+***
+class Dog
+{
+     String name="munna";
+     String color="black";
+     int cost=20000;
+     int age=6;
+     String breed="rottweiller";
+     void run(){
+     System.out.println("dog is running");
+     }
+     void sleep(){
+     System.out.println("dog is sleeping"); 
+     }
+     void walk(){
+     System.out.println("dog is walking");
+     }
+     void bark(){
+     System.out.println("dog is barking");
+     }
+     void cry(){
+     System.out.println("dog is crying");
+     }
+}
