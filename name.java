6b0fa9883127be75ea93227c60708de24829d34a
kushitha047 <22hr1a3234@mtieat.org>
2024-10-20 18:39:12 +0530

    Create name.java

---
+++ name.java
@@ -0,0 +1,14 @@
+***
+print name in 5 times
+***
+class Name
+{
+    public static void main(String args[])
+    {
+	int i=0;
+        for(i=0;i<5;i++)
+	{
+            System.out.println("KUSHITHA");
+	}
+    }
+}
