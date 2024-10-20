a3f00b89431888b881ff96c49bf80c24a74d14d2
kushitha047 <22hr1a3234@mtieat.org>
2024-10-20 18:38:19 +0530

    Create num.java

---
+++ num.java
@@ -0,0 +1,16 @@
+***
+print numbers in reverse order
+***
+class Num
+{
+    public static void main(String args[])
+    {
+        int i=5;
+	do
+	{
+            System.out.println(i);
+	    i--;
+	}
+	while(i>=1);
+     }
+}
