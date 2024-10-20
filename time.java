00fc80254f42b48ada8764a5452dbca800f3acc2
kushitha047 <22hr1a3234@mtieat.org>
2024-10-20 18:33:40 +0530

    Create time.java

---
+++ time.java
@@ -0,0 +1,22 @@
+***
+wishes in different timing
+***
+class Test
+{
+public static void main(String args[])
+{
+float time=10;
+if(time<12)
+{
+System.out.println("Good Morning");
+}
+else if(time>=12 && time<16)
+{
+System.out.println("Good Afternoon");
+}
+else
+{
+System.out.println("Good Evening");
+}
+}
+}
