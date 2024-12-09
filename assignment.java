bd0901c60261337cf18c8608e9c730d8f86d39d1
kushitha047 <22hr1a3234@mtieat.org>
2024-12-09 21:56:36 +0530

    assignment.java

---
+++ assignment.java
@@ -0,0 +1,22 @@
+***
+assignment operator 
+output:
+15
+10
+50
+10
+0
+***
+class assignment
+{
+public static void main(String args[])
+{
+int a=10;
+int b=5;
+System.out.println(a+=b);
+System.out.println(a-=b);
+System.out.println(a*=b);
+System.out.println(a/=b);
+System.out.println(a%=b);
+}
+}
