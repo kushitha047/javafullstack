dce3cb88ec9e3ebfbdbd69b619e393c04d53ec8c
kushitha047 <22hr1a3234@mtieat.org>
2024-12-09 22:20:41 +0530

    pattern7.java

---
+++ pattern7.java
@@ -0,0 +1,23 @@
+***
+pattern7
+output:
+11111
+22222
+33333
+44444
+55555
+***
+class pattern7
+{
+public static void main(String args[])
+{
+for(int i=1;i<=5;i++)
+{
+for(int j=1;j<=5;j++)
+{
+System.out.print(i);
+}
+System.out.println();
+}
+}
+}
