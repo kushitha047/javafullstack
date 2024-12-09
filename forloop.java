2571f9a093578c474b5240cac7fb8f49bb122c81
kushitha047 <22hr1a3234@mtieat.org>
2024-12-09 19:30:11 +0530

    forloop.java

---
+++ forloop.java
@@ -0,0 +1,18 @@
+***
+for loop
+output:
+KUSHITHA
+KUSHITHA
+KUSHITHA
+KUSHITHA
+KUSHITHA
+***
+class for1
+{
+    public static void main(String args[])
+    {
+         for(int i=0;i<5;i++){
+             System.out.println("KUSHITHA");
+	 }
+    }
+}
