665b5d5a83b2a1bdcd96382631433d4e8261311b
kushitha047 <22hr1a3234@mtieat.org>
2024-12-09 22:06:27 +0530

    pattern2.java

---
+++ pattern2.java
@@ -0,0 +1,23 @@
+***
+pattern2
+output:
+# # # # #
+# # # # #
+# # # # #
+# # # # #
+# # # # #
+***
+class pattern2
+{
+public static void main(String args[])
+{
+for(int i=0;i<5;i++)
+{
+for(int j=0;j<5;j++)
+{
+System.out.print("# ");
+}
+System.out.println();
+}
+}
+}
