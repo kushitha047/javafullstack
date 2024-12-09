a0a8dd31d04116537a7378987a19041098ed0a97
kushitha047 <22hr1a3234@mtieat.org>
2024-12-09 20:30:46 +0530

    Create ifelsecond.java

---
+++ ifelsecond.java
@@ -0,0 +1,20 @@
+***
+if else condition
+output:
+not eligible for voting
+***
+class ifelse
+{
+public static void main(String args[])
+{
+int age=17;
+if(age>=18)
+{
+System.out.println("eligible for voting");
+}
+else
+{
+System.out.println("not eligible for voting");
+}
+}
+}
