3c68f937c2489790edc2fa9a49d1fd2421c2e16c
kushitha047 <22hr1a3234@mtieat.org>
2024-12-09 21:06:27 +0530

    unary1.java

---
+++ unary1.java
@@ -0,0 +1,16 @@
+***
+unary operator
+output:
+22
+21
+***
+class unary1
+{
+public static void main(String args[])
+{
+int a=10;
+int b=10;
+System.out.println(a++ + ++a);
+System.out.println(b++ + b++);
+}
+}
