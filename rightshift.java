bf6e6e2eabe52b0db6c6be616499d88a7170d045
kushitha047 <22hr1a3234@mtieat.org>
2024-12-09 21:14:14 +0530

    rightshift.java

---
+++ rightshift.java
@@ -0,0 +1,16 @@
+***
+right operator
+output:
+2
+5
+2
+***
+class rightshift
+{
+public static void main(String args[])
+{
+System.out.println(10>>2);
+System.out.println(20>>2);
+System.out.println(20>>3);
+}
+}
