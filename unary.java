3175757c430f8f31df6acf156028d5483ce36d68
kushitha047 <22hr1a3234@mtieat.org>
2024-12-09 21:04:09 +0530

    unary.java

---
+++ unary.java
@@ -0,0 +1,19 @@
+***
+unary operator
+output:
+10
+10
+11
+11
+***
+class unary
+{
+public static void main(String args[])
+{
+int a=10;
+System.out.println(a++);
+System.out.println(--a);
+System.out.println(++a);
+System.out.println(a--);
+}
+}
