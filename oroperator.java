5f608b140d3074a0051b58670c86f852916ddb2d
kushitha047 <22hr1a3234@mtieat.org>
2024-12-09 21:37:37 +0530

    oroperator.java

---
+++ oroperator.java
@@ -0,0 +1,17 @@
+***
+OR operator
+output:
+true
+true
+***
+class or
+{
+public static void main(String args[])
+{
+int a=10;
+int b=5;
+int c=10;
+System.out.println(a>b || a<c);
+System.out.println(a>b | a<c);
+}
+}
