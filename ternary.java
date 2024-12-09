a8b3ae62584dcc9052dd18533e824d87cc70d3b4
kushitha047 <22hr1a3234@mtieat.org>
2024-12-09 21:44:55 +0530

    ternary.java

---
+++ ternary.java
@@ -0,0 +1,18 @@
+***
+ternary operator
+output:
+2
+5
+***
+class ternary
+{
+public static void main(String args[])
+{
+int a=2;
+int b=5;
+int min=(a<b)?a:b;
+int max=(a>b)?a:b;
+System.out.println(min);
+System.out.println(max);
+}
+}
