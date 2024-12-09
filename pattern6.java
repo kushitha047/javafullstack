7b632bce0efa88f77e0e2a63d4a989ec885f6e09
kushitha047 <22hr1a3234@mtieat.org>
2024-12-09 22:18:13 +0530

    pattern6.java

---
+++ pattern6.java
@@ -0,0 +1,27 @@
+***
+pattern6
+output:
+          #
+        # #
+      # # #
+    # # # #
+  # # # # #
+***
+class pattern6
+{
+public static void main(String args[])
+{
+for(int i=1;i<=5;i++)
+{
+for(int j=i;j<=5;j++)
+{
+System.out.print("  ");
+}
+for(int j=1;j<=i;j++)
+{
+System.out.print("# ");
+}
+System.out.println();
+}
+}
+}
