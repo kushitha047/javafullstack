18c7149f005b76d8cc2dcd8c5df7edc5a152bb67
kushitha047 <22hr1a3234@mtieat.org>
2024-12-09 22:31:34 +0530

    pattern11.java

---
+++ pattern11.java
@@ -0,0 +1,27 @@
+***
+pattern11
+output:
+          *
+        * * *
+      * * * * *
+    * * * * * * *
+  * * * * * * * * *
+***
+class pattern11
+{
+public static void main(String args[])
+{
+for(int i=1;i<=5;i++)
+{
+for(int j=i;j<=5;j++)
+{
+System.out.print("  ");
+}
+for(int j=1;j<=(2*i-1);j++)
+{
+System.out.print("* ");
+}
+System.out.println();
+}
+}
+}
