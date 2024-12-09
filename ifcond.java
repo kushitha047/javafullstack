eb329c0e9653755d52c3ffbfaf361cb1fccc1f8e
kushitha047 <22hr1a3234@mtieat.org>
2024-12-09 20:26:07 +0530

    ifcond.java

---
+++ ifcond.java
@@ -0,0 +1,16 @@
+***
+if condition
+output:
+eligible for voting
+***
+class ifcond
+{
+public static void main(String args[])
+{
+int age=18;
+if(age>=18)
+{
+System.out.println("eligible for voting");
+}
+}
+}
