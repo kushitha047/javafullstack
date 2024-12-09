39fcf4018304b19da3ac84895a5c220bbc4e32af
kushitha047 <22hr1a3234@mtieat.org>
2024-12-09 20:39:49 +0530

    elseifcond.java

---
+++ elseifcond.java
@@ -0,0 +1,24 @@
+***
+else if condition
+output:
+good morning
+***
+class elseif
+{
+public static void main(String args[])
+{
+int time=10;
+if(time>1 && time<12)
+{
+System.out.println("good morning");
+}
+else if(time>=12 && time<16)
+{
+System.out.println("good afternoon");
+}
+else
+{
+System.out.println("good evening");
+}
+}
+}
