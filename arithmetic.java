4e2091ca39ec640e61a7338a97d54324f25bf219
kushitha047 <22hr1a3234@mtieat.org>
2024-12-09 20:57:02 +0530

    arithmetic.java

---
+++ arithmetic.java
@@ -0,0 +1,22 @@
+***
+arithmetic operations
+output:
+15
+5
+50
+2
+0
+***
+class arithmetic
+{
+public static void main(String args[])
+{
+int x=10;
+int y=5;
+System.out.println(x+y);
+System.out.println(x-y);
+System.out.println(x*y);
+System.out.println(x/y);
+System.out.println(x%y);
+}
+}
