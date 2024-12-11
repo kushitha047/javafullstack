639c1585a63083e93aaac3b24cef9e506130c5af
kushitha047 <22hr1a3234@mtieat.org>
2024-12-11 20:42:25 +0530

    Create reverse.java

---
+++ reverse.java
@@ -0,0 +1,19 @@
+***
+reverse order
+output:
+gninrom doog olleh
+***
+package arrays;
+
+public class k6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String words="hello good morning";
+		for(int i=words.length()-1;i>=0;i--) {
+	         System.out.print(words.charAt(i));
+		}
+
+	}
+
+}
