eb2ba899d69862803e41b0f0f3b61c89860e372f
kushitha047 <22hr1a3234@mtieat.org>
2024-12-11 20:29:05 +0530

    Create summinmax.java

---
+++ summinmax.java
@@ -0,0 +1,36 @@
+***
+sum,min,max
+output:
+minimum number:5
+maximum number:70
+sum of the given numbers:105
+***
+class sum {
+
+	public static void main(String[] args) {
+		int sum=0;
+		// TODO Auto-generated method stub
+		int[] a= {10,20,5,70};
+		int min=a[0];
+		int max=a[0];
+		for(int i=0;i<a.length;i++)
+		{
+		    if (a[i]<min)
+		    {
+			    min=a[i];
+		    }
+		    if(a[i]>max)
+		    {
+		    	max=a[i];
+		    }
+		}
+		System.out.println("minimum number:"+min);
+		System.out.println("maximum number:"+max);
+		for(int i=0;i<a.length;i++)
+		{
+			sum+=a[i];
+		}
+		System.out.println("sum of the given numbers:"+sum);
+	}
+
+}
