237b3ad9d45dc7fc18301847d0f902ecc5fe0f78
kushitha047 <22hr1a3234@mtieat.org>
2024-12-11 20:40:49 +0530

    Create concat2.java

---
+++ concat2.java
@@ -0,0 +1,21 @@
+***
+concat function in string 
+output:
+stringfall
+stringwinter
+***
+package arrays;
+
+public class k5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s1=new String("spring");
+        s1.concat("summer");
+        String s2=s1.concat("winter");
+        s1=s1.concat("fall");
+        System.out.println(s1);
+        System.out.println(s2);
+	}
+
+}
