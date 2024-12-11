0fd4f858e337823fef0e377eba24f238cf816996
kushitha047 <22hr1a3234@mtieat.org>
2024-12-11 20:36:43 +0530

    Create count.java

---
+++ count.java
@@ -0,0 +1,22 @@
+***
+count
+output:
+18
+18
+***
+package arrays;
+
+public class k3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String words="hello good morning";
+		int count=0;
+	    for(int i=0;i<words.length();i++) {
+	    	count++;
+	    }
+        System.out.println(count);
+        System.out.println(words.length());
+	}
+
+}
