00d3f5db3834caeca36d3f84c3a98fa28eb2b22d
kushitha047 <22hr1a3234@mtieat.org>
2024-12-12 18:41:37 +0530

    Create equal2.java

---
+++ equal2.java
@@ -0,0 +1,19 @@
+***
+equals method in StrintBuffer class
+output:
+false
+false
+***
+package arrays;
+
+public class a3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		StringBuffer s1=new StringBuffer("hello");
+        StringBuffer s2=new StringBuffer("hello");
+        System.out.println(s1==s2);
+        System.out.println(s1.equals(s2));
+	}
+
+}
