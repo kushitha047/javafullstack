6cdebbe2acc3475f937695eaac33690d253a46f7
kushitha047 <22hr1a3234@mtieat.org>
2024-12-12 18:36:08 +0530

    Create equal.java

---
+++ equal.java
@@ -0,0 +1,19 @@
+***
+equals method in string
+output:
+false
+true
+***
+package arrays;
+
+public class a1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        String s1=new String("hello");
+        String s2=new String("hello");
+        System.out.println(s1==s2);
+        System.out.println(s1.equals(s2));
+	}
+
+}
