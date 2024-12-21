888c4b3f51c99e49dd8f07009757fa4f5d9316d2
kushitha047 <22hr1a3234@mtieat.org>
2024-12-21 11:48:16 +0530

    Create treeset.java

---
+++ treeset.java
@@ -0,0 +1,24 @@
+***
+treeset program
+output:
+[10, 50, 75]
+***
+package racecondition;
+import java.util.TreeSet;
+public class f12 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TreeSet ts=new TreeSet();
+		ts.add(10);
+		ts.add(75);
+		ts.add(50);
+		ts.ceiling(50);
+		ts.higher(50);
+		ts.floor(40);
+		ts.lower(40);
+		System.out.println(ts);
+
+	}
+
+}
