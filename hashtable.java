44c31c259f90bfb63e470298f8b3ab3137be2975
kushitha047 <22hr1a3234@mtieat.org>
2024-12-21 11:55:44 +0530

    Create hashtable.java

---
+++ hashtable.java
@@ -0,0 +1,26 @@
+***
+hashtable program
+output:
+{10=anu, 19=soumya, 5=ammu, 15=kavya, 13=harshi, 12=kushi, 11=yogesh}
+***
+package racecondition;
+
+import java.util.Hashtable;
+
+public class f17 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Hashtable ht=new Hashtable();
+		ht.put(11, "yogesh");
+		ht.put(12,"kushi");
+		ht.put(15,"kavya");
+		ht.put(13,"harshi");
+		ht.put(10,"anu");
+		ht.put(19,"soumya");
+		ht.put(5,"ammu");
+		System.out.println(ht);
+
+	}
+
+}
