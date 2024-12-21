a51ab36e8351248308e66f28426699a808e28fcb
kushitha047 <22hr1a3234@mtieat.org>
2024-12-21 11:49:46 +0530

    Create hashset.java

---
+++ hashset.java
@@ -0,0 +1,22 @@
+***
+hashset program
+output:
+[20, 4, 39, 55, 10]
+***
+package racecondition;
+import java.util.HashSet;
+public class f13 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		HashSet hs=new HashSet();
+		hs.add(20);
+		hs.add(39);
+		hs.add(10);
+		hs.add(4);
+		hs.add(39);
+		hs.add(55);
+		System.out.println(hs);
+	}
+
+}
