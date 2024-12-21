39e8c5cac3c0d2eff425912a5b90f3e2341a78d8
kushitha047 <22hr1a3234@mtieat.org>
2024-12-21 11:50:57 +0530

    Create linkedhashset.java

---
+++ linkedhashset.java
@@ -0,0 +1,23 @@
+***
+linkedhashset program
+output:
+[20, 39, 10, 4, 55]
+***
+package racecondition;
+import java.util.LinkedHashSet;
+public class f14 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		LinkedHashSet lhs=new LinkedHashSet();
+		lhs.add(20);
+		lhs.add(39);
+		lhs.add(10);
+		lhs.add(4);
+		lhs.add(39);
+		lhs.add(55);
+		System.out.println(lhs);
+
+	}
+
+}
